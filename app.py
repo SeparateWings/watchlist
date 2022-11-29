0a63724779c616a6c1c9fd1019b9a27939576cd3
SeparateWings <jiayaozon@163.com>
2022-11-29 23:45:59 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-from flask import Flask
-from flask import url_for
+from flask import Flask, url_for, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/home')
-@app.route('/index')
-def hello():
-    return (
-        '<h1>Welcome to My Watchlist!</h1><img src="http://helloflask.com/totoro.gif">'
-    )
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 
 @app.route('/user/<name>')
@@ -26,3 +21,18 @@ def test_url_for():
     print(url_for('test_url_for'))
     print(url_for('test_url_for', num=2))
     return '<h1>Test page</h1>'
+
+
+name = 'SW'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
